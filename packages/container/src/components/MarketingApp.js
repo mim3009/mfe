c262c1562ef00f452fd26ac5e1e684c5458624aa
Roman Zhyhulskyi <roman.zhyhulskyi@valtech.com>
2023-06-30 10:56:06 +0300

    initial

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,14 @@
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    }, [ref]);
+
+    return (
+        <div ref={ref}></div>
+    );
+};
