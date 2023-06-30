b64c522441075bca6a9c43e7f7c88564019e2761
Roman Zhyhulskyi <roman.zhyhulskyi@valtech.com>
2023-06-30 11:40:50 +0300

    container deploy

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const ModuleFedarationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFedarationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJSON.dependencies
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
